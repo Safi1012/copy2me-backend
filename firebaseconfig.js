742e03d3a03fda52bf54e82ce7fa845b6f70db62
Filipe Santos Correa <filipe@Filipes-MacBook-Pro-2.local>
2017-02-24 11:50:18 +0100

    Implemented access to Firebase in order to fetch push-subscriptions.

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,6 +1,17 @@
-var admin = require("firebase-admin");
+// let admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert("path/to/serviceAccountKey.json"),
-  databaseURL: "https://clipme-32a80.firebaseio.com"
-});
+// module.exports = function () {
+
+//   admin.initializeApp({
+//     credential: admin.credential.cert("./firebase_serviceAccountKey.json"),
+//     databaseURL: "https://clipme-32a80.firebaseio.com"
+//   });
+
+//   let defaultAuth = admin.auth();
+//   let defaultDatabase = admin.database();
+
+//   console.log(defaultAuth);
+//   console.log(defaultDatabase);
+
+//   return admin;
+// }
--- index.js
+++ index.js
@@ -1,11 +1,93 @@
 var express = require('express')
 var app = express()
+var request = require('request');
+var firebase = require('firebase-admin');
+var API_KEY = 'AAAA6R8joXo:APA91bG7y0eZNmFItOlKu47cpnclWV50NSylKPttKBIle1Mt57rM2RUSLH7AMHTqdRgKAChF_1UhSDs2623es5P-5_y9BwlbQVZtwVuVibYqOa7hY_mqob4jlIyMA13ew5BkDMVNkLSkOjSkR3P4i4ANCMOHBRnGLA';
 
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!')
 })
 
 app.get('/', (req, res) => {
-  console.log('123456789')
   res.send('Hello Arne!')
+  listenToChanges();
 })
+
+firebase.initializeApp({
+  credential: firebase.credential.cert('./firebase_serviceAccountKey.json'),
+  databaseURL: 'https://clipme-32a80.firebaseio.com'
+});
+
+// listenToChanges();
+
+function listenToChanges() {
+  let links = firebase.database().ref().child('links');
+
+  links.on('child_added', data => {
+    listenToNewHistoryItems(data.key);
+  });
+}
+
+function listenToNewHistoryItems(uid) {
+  console.log('links/' + uid + '/history');
+
+  firebase.database().ref().child('links/' + uid + '/history/').on('child_added', item => {
+    let notificationSent = item.val()['notification-sent'];
+    let excludeDeviceAuthFromPush = item.val()['push-auth'];
+
+    if (!notificationSent) {
+      sentAuthDataToFCM(uid, excludeDeviceAuthFromPush)
+    }
+  });
+}
+
+function sentAuthDataToFCM(uid, excludeDeviceAuthFromPush) {
+  console.log(uid + excludeDeviceAuthFromPush);
+
+
+  firebase.database().ref().child('links/' + uid + '/push-subscriptions/').once('value', subscriptions => {
+    console.log(subscriptions);
+    console.log(subscriptions.val());
+
+    for (var key in subscriptions.val()) {
+      let auth = subscriptions.val()[key].auth;
+      let endpoint = subscriptions.val()[key].endpoint;
+      let p256dh = subscriptions.val()[key].p256dh;
+
+      // sent request to FCM
+      sendNotificationToUser(auth, endpoint, p256dh);
+
+      // set sentNotification to true -> after sending
+    }
+  });
+}
+
+function sendNotificationToUser(auth, endpoint, p256dh) {
+  console.log(API_KEY);
+  message = 'Test';
+  let registrationID = endpoint;
+
+  if (endpoint.startsWith('https://android.googleapis.com/gcm/send')) {
+    let endpointParts = endpoint.split('/');
+    registrationID = endpointParts[endpointParts.length - 1];
+  }
+
+  request({
+    url: 'https://android.googleapis.com/gcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type': ' application/json',
+      'Authorization': 'key=' + API_KEY,
+    },
+    body: JSON.stringify({
+      'registration_ids': [registrationID]
+    })
+  }, function(error, response, body) {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode >= 400) {
+      console.error('HTTP Error: ' + response.statusCode + ' - ' + response.statusMessage);
+    }
+  });
+
+}
